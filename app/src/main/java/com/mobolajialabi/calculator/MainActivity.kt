e42db333efecb3ab889668144bb90f2d3dacab47
Alabi Bolaji <alabibolaji78@gmail.com>
2021-05-19 23:55:26 +0100

    implements basic calculator functionality

--- app/src/main/java/com/mobolajialabi/calculator/MainActivity.kt
+++ app/src/main/java/com/mobolajialabi/calculator/MainActivity.kt
@@ -10,10 +10,9 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private var curValue = 1
-    private var dupValue = 1
-    private var bb = arrayListOf<Int>()
-    private var curOperator = 0
+    private var curValue : Double = 0.0
+    private var calculated = 0
+    private var curOperators = arrayListOf<Int>()
 
     // + == 1
     // - == 2
@@ -39,69 +38,84 @@ class MainActivity : AppCompatActivity() {
         binding.eight.setOnClickListener { showNumber(8) }
         binding.nine.setOnClickListener { showNumber(9) }
         binding.zero.setOnClickListener { showNumber(0) }
-        binding.plus.setOnClickListener {
-            showOperator("+")
-        }
+        binding.plus.setOnClickListener { showOperator("+") }
         binding.minus.setOnClickListener { showOperator("-") }
         binding.divide.setOnClickListener { showOperator("/") }
         binding.multiply.setOnClickListener { showOperator("*") }
-        binding.equals.setOnClickListener { calculate() }
+        binding.equals.setOnClickListener { calculate()}
+        binding.erase.setOnClickListener { erase(binding.screen.text.toString()) }
     }
 
-    private fun add() {
-        curValue = binding.screen.text.toString().toInt()
+    private fun add(val1: Double, val2: Double) : Double {
+        return val1 + val2
+    }
+    
+    private fun subtract(val1 :  Double, val2 :  Double) :  Double {
+        return val1 - val2
+    }
+    
+    private fun multiply(val1 :  Double, val2 :  Double) :  Double {
+        return val1 * val2
+    }
+    
+    private fun divide(val1 :  Double, val2 :  Double) :  Double {
+        return val1 / val2
+    }
+    
+    private fun calculate() {
+        calculated = 1
+
+        val x = binding.screen.text.toString().split('+', '-', '*', '/')
+        curValue = x[0].toDouble()
+
+        for ((count, _) in (0 until x.lastIndex).withIndex()) {
+            curValue = when (curOperators[count]) {
+                1 -> add(curValue, x[count + 1].toDouble())
+                2 -> subtract(curValue, x[count + 1].toDouble())
+                3 -> multiply(curValue, x[count + 1].toDouble())
+                4 -> divide(curValue, x[count + 1].toDouble())
+
+                else -> 0.0
+            }
+        }
 
         binding.screen.text = ""
-
-//        binding.screen.text = getString(R.string.value2, binding.screen.text, "+")
+        binding.screen.text = curValue.toString()
+        curOperators.clear()
     }
+    
+    private fun showOperator(op : String)  {
+        val v = binding.screen.text.toString()
 
+        if (!v.endsWith('/') && !v.endsWith('*') && !v.endsWith('+')  && !v.endsWith('-') ) {
+            binding.screen.text = getString(R.string.value2, binding.screen.text, op)
+
+            curOperators.add(when (op) {
+                "+" -> 1
+                "-" -> 2
+                "*" -> 3
+                "/" -> 4
+                else -> 0
+            })
+        }
+
+    }
+    
     private fun showNumber(no : Int) {
-        binding.screen.text = getString(R.string.value, binding.screen.text, no)
-    }
+        if (calculated != 0) {
+            binding.screen.text = ""
+            calculated = 0
+        }
 
-    private fun showOperator(va : String) {
-        curValue = binding.screen.text.toString().toInt()
-
-//        binding.screen.text = getString(R.string.value2, binding.screen.text, va)
-
-        if (curOperator != 0)  {
-            when (curOperator) {
-                1 -> curValue += binding.screen.text.toString().toInt()
-                2 -> curValue -= binding.screen.text.toString().toInt()
-                3 -> curValue *= binding.screen.text.toString().toInt()
-                4 -> curValue /= binding.screen.text.toString().toInt()
-            }
+        if (binding.screen.text != "0") {
+            binding.screen.text = getString(R.string.value, binding.screen.text, no)
+        } else {
+           binding.screen.text = no.toString()
         }
     }
 
-    private fun calculate() {
-        val vv = binding.screen.text
-        val list = vv.split('/', '*', '+', '-')
-
-//        for (char in vv) {
-//            if (char.toInt() in=)
-//            if (char == '/') {
-//                for (c in )
-//            }
-//        }
-
-//        binding.screen.text = list.toString()
-
-        var v : Int
-
-//        if (vv.contains('*')) {
-//            val list = vv.split('*')
-//
-//            if (!vv.contains('/') && !vv.contains('+') && !vv.contains('-')) {
-//                for (element in list) {
-////                    v = curValue
-//
-//                    curValue *= element.toInt()
-//                }
-//
-//                binding.screen.text = curValue.toString()
-//            }
-//        }
+    private fun erase(str : String){
+        val x = str.dropLast(1)
+        binding.screen.text = x
     }
 }
